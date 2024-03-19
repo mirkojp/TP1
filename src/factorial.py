4239d33dabcdd8549ccae1827a374f48df6fc3e5
mirkojp <mirkopavan88878@gmail.com>
2024-03-19 19:36:09 -0300

    comments

--- src/factorial.py
+++ src/factorial.py
@@ -1,28 +1,37 @@
-
-#*-------------------------------------------------------------------------*
-#* factorial.py                                                            *
-#* calcula el factorial de un número                                       *
-#* Dr.P.E.Colla (c) 2022                                                   *
-#* Creative commons                                                        *
-#*-------------------------------------------------------------------------*
+# *-------------------------------------------------------------------------*
+# * factorial.py                                                            *
+# * calcula el factorial de un número                                       *
+# * Dr.P.E.Colla (c) 2022                                                   *
+# * Creative commons                                                        *
+# *-------------------------------------------------------------------------*
 import sys
+
+# Funcion para calcular el factorial de un numero
 def factorial(num): 
+    # Manejo de caso de numeros negativos
     if num < 0: 
         print("Factorial de un número negativo no existe")
 
-    elif num == 0: 
+    # Manejo de caso de 0
+    elif num == 0:
         return 1
-        
+
+    # Manejo de caso numero enteros positivos
     else: 
         fact = 1
+        # Iteracion calculo factorial
         while(num > 1): 
             fact *= num 
             num -= 1
         return fact 
 
+#Revisar si el argumento no esta vacio
 if len(sys.argv) == 0:
    print("Debe informar un número!")
    sys.exit()
-num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
 
+#Convertir argumento en numero entero
+num=int(sys.argv[1])
+
+#Imprimimos el resultados
+print("Factorial ",num,"! es ", factorial(num)) 
--- src/primos.py
+++ src/primos.py
@@ -1,16 +1,19 @@
 
 # Python program to display all the prime numbers within an interval
 
+#Limits of the interval
 lower = 1
 upper = 100
 
 print("Prime numbers between", lower, "and", upper, "are:")
 
+# Iterate through all numbers of the interval
 for num in range(lower, upper + 1):
-   # all prime numbers are greater than 1
+   # All prime numbers are greater than 1
    if num > 1:
+       #Check if the numbers is divisible by any number less than itself
        for i in range(2, num):
-           if (num % i) == 0:
+           if (num % i) == 0: #If it is divisible it is not prime
                break
-       else:
+       else:#If it is not divisible is prime
            print(num)
